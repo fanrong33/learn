007dc37dc73cdf1d23e4cbe54ff6a778c1f52661
fanrong33 <fanrong33@qq.com>
2015-03-24 17:58:03 +0800

    新增录音功能和清空历史记录

---
+++ helloh5plus/js/common.js
@@ -0,0 +1,101 @@
+
+// 扩展API准备完成后要执行的操作
+function plusReady(){
+	// 隐藏滚动条
+	plus.webview.currentWebview().setStyle({scrollIndicator:'none'});
+	
+}
+
+if(window.plus){
+    plusReady();
+}else{ 
+    document.addEventListener("plusready", plusReady, false);
+}
+
+// DOMContentLoaded事件处理
+var is_domready=false;
+document.addEventListener('DOMContentLoaded',function(){
+	is_domready=true;
+	
+	document.getElementById('J_back').addEventListener('tap', function(){
+		back(true);
+	});
+	
+	g_init();
+},false);
+
+// 处理返回事件
+function back(hide){
+	if(window.plus){
+		var ws = plus.webview.currentWebview();
+		if(hide){
+			console.info(ws.id+' hide');
+			ws.hide('auto', 300);
+		}else{
+			console.info(ws.id+' close');
+			ws.close('auto', 300);
+		}
+	}else if(history.length>1){
+		history.back();
+	}else{
+		window.close();
+	}
+}
+
+// 通用元素对象
+var _dout_;
+var _dcontent_;
+function g_init(){
+	_dout_=document.getElementById("output");
+	_dcontent_=document.getElementById("dcontent");
+}
+
+// 清空输出内容
+function out_clean(){
+	_dout_.innerHTML="";
+	_dout_.scrollTop=0;//在iOS8存在不滚动的现象
+};
+// 输出内容
+function out_set(s){
+	_dout_.innerHTML=s+"<br/>";
+	(0==_dout_.scrollTop)&&(_dout_.scrollTop=1);//在iOS8存在不滚动的现象
+};
+
+// 输出行内容
+function out_line(s){
+	_dout_.innerHTML+=s+"<br/>";
+	(0==_dout_.scrollTop)&&(_dout_.scrollTop=1);//在iOS8存在不滚动的现象
+};
+
+
+// 格式化时长字符串，格式为"HH:MM:SS"
+function timeToStr(ts){
+	if(isNaN(ts)){
+		return "--:--:--";
+	}
+	var h=parseInt(ts/3600);
+	var m=parseInt((ts%3600)/60);
+	var s=parseInt(ts%60);
+	return (ultZeroize(h)+":"+ultZeroize(m)+":"+ultZeroize(s));
+};
+
+// 格式化日期时间字符串，格式为"YYYY-MM-DD HH:MM:SS"
+function dateToStr(d){
+	return (d.getFullYear()+"-"+ultZeroize(d.getMonth()+1)+"-"+ultZeroize(d.getDate())+" "+ultZeroize(d.getHours())+":"+ultZeroize(d.getMinutes())+":"+ultZeroize(d.getSeconds()));
+};
+
+/**
+ * zeroize value with length(default is 2).
+ * @param {Object} v
+ * @param {Number} l
+ * @return {String} 
+ */
+function ultZeroize(v,l){
+	var z="";
+	l=l||2;
+	v=String(v);
+	for(var i=0;i<l-v.length;i++){
+		z+="0";
+	}
+	return z+v;
+};
